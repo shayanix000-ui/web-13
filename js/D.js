96040cccf1973cefd751bd91f4678afde84ac746
shayanix788 <shayanix000@gmail.com>
2025-11-25 16:05:57 +0330

    web-13-2

--- js/D.js
+++ js/D.js
@@ -1,14 +1,7 @@
 window.addEventListener("load",function(){
-    var swiper = new Swiper(".mySwiper", {
-      spaceBetween: 50,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-    });
-    var swiper2 = new Swiper(".mySwiper2", {
-      direction: "vertical",
-      spaceBetween: 50,
+     var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 3,
+      spaceBetween: 30,
       pagination: {
         el: ".swiper-pagination",
         clickable: true,
